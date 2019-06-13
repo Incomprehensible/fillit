ee6e85f299a52dac4ca3cc603e3ec4d166031f7c
Nikolay Nikitin <nikitin.na@phystech.edu>
2019-06-13 15:36:52 +0300

    forgotten son

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: crycherd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 17:34:56 by crycherd          #+#    #+#             */
-/*   Updated: 2019/06/13 13:59:45 by crycherd         ###   ########.fr       */
+/*   Updated: 2019/06/13 15:36:23 by crycherd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,36 @@
 
 # include "libft.h"
 
-typedef struct		s_list
+typedef struct		s_map
 {
 	char			sym;
 	char			**figs;
 	int				offset_x;
 	int				offset_y;
 	char			**t_map;
-	struct s_list	*next;
-	struct s_list	*previous;
+	struct s_map	*next;
+	struct s_map	*previous;
 }					t_map;
 
+void				*ft_arrmemdel(void **ap);
+int					ft_count_type(t_map *map);
+int					ft_check_type(t_map *map, int x, int y, int fig);
+int					ft_get_type1(t_map *map);
+int					ft_get_type2(t_map *map);
+int					fillit(t_map *map, int num);
+void				ft_set(t_map *map, int x, int y);
+void				ft_updtxy(t_map *map, int num);
+int					subst(t_map *map, int num, int flag);
+int					ft_check_offset(t_map *map, int num, int flag);
+int					ft_remove(t_map *alst);
+int					find_x(char **fig, int num);
+int					find_y(char **fig, int num);
+void				ft_getmap(int size, t_map *lst);
+void				ft_check_map(t_map *map, int num);
+int					ft_sqrt(int nb);
+void				ft_lstdel_one(t_map **alst);
+int					ft_printlst(t_map *valid);
+char				**ft_arrcopy(char **arr);
 void				*ft_arrmemdel(void **ap);
 void				free_buf(char **buf, int i);
 t_map				*ft_input(char *file_name);
